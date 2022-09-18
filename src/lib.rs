85a2e32586a528cce91002591dbcd2cb01fa2e20
Andrew Korzhuev <korzhuev@andrusha.me>
2022-09-18 11:49:38 +0300

    Numpy sample

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,41 @@
-use pyo3::prelude::*;
+use numpy::ndarray::{ArrayD, ArrayViewD, ArrayViewMutD};
+use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn};
+use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
 #[pymodule]
 fn numpy_maxminddb(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    // example using immutable borrows producing a new array
+    fn axpy(a: f64, x: ArrayViewD<'_, f64>, y: ArrayViewD<'_, f64>) -> ArrayD<f64> {
+        a * &x + &y
+    }
+
+    // example using a mutable borrow to modify an array in-place
+    fn mult(a: f64, mut x: ArrayViewMutD<'_, f64>) {
+        x *= a;
+    }
+
+    // wrapper of `axpy`
+    #[pyfn(m)]
+    #[pyo3(name = "axpy")]
+    fn axpy_py<'py>(
+        py: Python<'py>,
+        a: f64,
+        x: PyReadonlyArrayDyn<f64>,
+        y: PyReadonlyArrayDyn<f64>,
+    ) -> &'py PyArrayDyn<f64> {
+        let x = x.as_array();
+        let y = y.as_array();
+        let z = axpy(a, x, y);
+        z.into_pyarray(py)
+    }
+
+    // wrapper of `mult`
+    #[pyfn(m)]
+    #[pyo3(name = "mult")]
+    fn mult_py(_py: Python<'_>, a: f64, x: &PyArrayDyn<f64>) {
+        let x = unsafe { x.as_array_mut() };
+        mult(a, x);
+    }
+
     Ok(())
 }
