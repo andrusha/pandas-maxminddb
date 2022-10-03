6496367046e9f6ca2053351136b6abc991cf9be5
Andrew Korzhuev <korzhuev@andrusha.me>
2022-10-03 11:44:33 +0200

    Add columns enum and python conversions, fix build

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## Todo
 - [ ] Add type annotations https://maturin.rs/project_layout.html
 - [ ] Distribute for multiple python versions https://pyo3.rs/v0.17.1/building_and_distribution.html
-- 
+- [ ] Figure out GIL https://pyo3.rs/v0.17.1/types.html
+- [ ] Add error handling
+- [ ] Add mmdb as a submodule
 
 ## Develop
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,104 @@
+use std::collections::HashMap;
 use std::net::IpAddr;
-use numpy::{IntoPyArray, PyReadonlyArrayDyn};
-use pyo3::{pymodule, PyObject, PyResult, Python, ToPyObject, types::PyModule};
-use pyo3::types::{PyDict, PyString};
-use pyo3::prelude::*;
+use std::str::FromStr;
 
 use maxminddb::geoip2;
+use numpy::{IntoPyArray, PyArray1, PyReadonlyArray1, PyReadwriteArray1};
+use pyo3::{pymodule, PyObject, PyResult, Python, ToPyObject, types::PyModule};
+use pyo3::exceptions::PyKeyError;
+use pyo3::prelude::*;
+use pyo3::types::{PyDict, PyString};
+use thiserror::Error;
+
+// country=none_if_exception(lambda: geo["country"]["names"]["en"]),
+// state=none_if_exception(lambda: geo["subdivisions"][0]["names"]["en"]),
+// city=none_if_exception(lambda: geo["city"]["names"]["en"]),
+// postcode=none_if_exception(lambda: geo["postal"]["code"]),
+// longitude=none_if_exception(lambda: float(geo["location"]["longitude"])),
+// latitude=none_if_exception(lambda: float(geo["location"]["latitude"])),
+// accuracy_radius=none_if_exception(lambda: int(geo["location"]["accuracy_radius"])),
+
+// todo: into PyErr
+#[derive(Error, Debug)]
+enum PandasMaxmindError {
+  #[error("unknown geo column")]
+  ParseColumnError
+}
+
+impl From<PandasMaxmindError> for PyErr {
+  fn from(e: PandasMaxmindError) -> Self {
+    match e {
+      PandasMaxmindError::ParseColumnError => PyKeyError::new_err(e.to_string())
+    }
+  }
+}
+
+#[derive(Debug)]
+enum GeoColumn {
+  Country,
+  State,
+  City,
+  Postcode,
+  Longitude,
+  Latitude,
+  AccuracyRadius,
+}
+
+impl FromStr for GeoColumn {
+  type Err = PandasMaxmindError;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    use GeoColumn::*;
+
+    match s {
+      "country" => Ok(Country),
+      "state" => Ok(State),
+      "city" => Ok(City),
+      "postcode" => Ok(Postcode),
+      "longitude" => Ok(Longitude),
+      "latitude" => Ok(Latitude),
+      "accuracy_radius" => Ok(AccuracyRadius),
+      _ => Err(PandasMaxmindError::ParseColumnError)
+    }
+  }
+}
+
+impl IntoPy<PyObject> for GeoColumn {
+  fn into_py(self, py: Python<'_>) -> PyObject {
+    match self {
+      GeoColumn::Country => "country".into_py(py),
+      GeoColumn::State => "state".into_py(py),
+      GeoColumn::City => "city".into_py(py),
+      GeoColumn::Postcode => "postcode".into_py(py),
+      GeoColumn::Longitude => "longitude".into_py(py),
+      GeoColumn::Latitude => "latitude".into_py(py),
+      GeoColumn::AccuracyRadius => "accuracy_radius".into_py(py),
+    }
+  }
+}
+
+impl<'source> FromPyObject<'source> for GeoColumn {
+  fn extract(ob: &'source PyAny) -> PyResult<Self> {
+    let s = ob.extract::<&str>()?;
+
+    Ok(GeoColumn::from_str(s)?)
+  }
+}
 
 #[pyfunction]
 fn mmdb_geolocate<'py>(
   py: Python<'py>,
-  ips: PyReadonlyArrayDyn<PyObject>,
+  ips: PyReadonlyArray1<PyObject>,
+  columns: Vec<GeoColumn>
 ) -> &'py PyDict {
   let reader = maxminddb::Reader::open_readfile("GeoLite2-City.mmdb").unwrap();
+
+  println!("{:#?}", columns);
+
+  // let res: HashMap<GeoColumn, Vec<PyObject>> = HashMap::new();
+  // for c in columns.into_iter() {
+  //   res[c.into()] = ips.len();
+  // }
 
   let cities = ips
       .as_array()
